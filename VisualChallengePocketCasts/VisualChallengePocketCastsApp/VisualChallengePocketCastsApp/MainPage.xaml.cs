257165dbfc754b04b086893fc0613923017fba63
Samuel Schumacher <samuel.schumacher@daveramsey.com>
2021-01-04 14:02:01 -0600

    styling

--- VisualChallengePocketCasts/VisualChallengePocketCastsApp/VisualChallengePocketCastsApp/MainPage.xaml.cs
+++ VisualChallengePocketCasts/VisualChallengePocketCastsApp/VisualChallengePocketCastsApp/MainPage.xaml.cs
@@ -1,9 +1,55 @@
-﻿namespace VisualChallengePocketCastsApp
+﻿using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace VisualChallengePocketCastsApp
 {
     public partial class MainPage
     {
+        public List<Color> Podcasts { get; set; }
+        
         public MainPage()
         {
+            BindingContext = this;
+            
+            Podcasts = new List<Color>
+            {
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+                
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+                
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+                
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+                
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+                
+                Color.FromHex("#8a8a8a"),
+                Color.FromHex("#969696"),
+                Color.FromHex("#a3a3a3"),
+                Color.FromHex("#b0b0b0"),
+                Color.FromHex("#bdbdbd"),
+            };
+            
             InitializeComponent();
         }
     }
